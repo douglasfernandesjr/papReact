e1f333dfe8a8d709c43953a896b9c36117e4571a
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-08-30 20:34:14 -0300

    Adicionar projeto padrão

---
+++ olamundo/src/components/Title.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export class Title extends Component {
+    render() {
+        const titulo = "Olá, mundo!";
+        return <h1>{titulo}</h1>;
+    }
+}
