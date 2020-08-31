5a2f1546d47f07d95e67fda8c663a8efcce60aed
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-08-30 21:15:41 -0300

    add exports default

--- olamundo/src/App.js
+++ olamundo/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Title/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- olamundo/src/components/Title.jsx
+++ olamundo/src/components/Title.jsx
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Title extends Component {
-    render() {
-        const titulo = "Olá, mundo!";
-        return <h1>{titulo}</h1>;
-    }
+  render() {
+    const titulo = "Olá, mundo!";
+    return <h1>{titulo}</h1>;
+  }
 }
+
+export default Title;
