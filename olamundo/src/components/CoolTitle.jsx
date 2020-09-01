b69f43eec499cd70feb894419eb36edd8828c4cd
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-08-31 22:20:37 -0300

    Atualizar CoolTitle

--- olamundo/src/components/CoolTitle.jsx
+++ olamundo/src/components/CoolTitle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function CoolTitle(props) {
   const titulo = props.info ?? "Olá, mundo!";
-  return <h1>{titulo}</h1>;
+  return (<h1>{titulo}</h1>);
 }
 
 export default CoolTitle;
