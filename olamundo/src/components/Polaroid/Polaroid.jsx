97ee3594d69dd39949cd22f8ec33c9782cc51705
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-09-04 13:55:07 -0300

    atualizar imports

--- olamundo/src/components/Polaroid/Polaroid.jsx
+++ olamundo/src/components/Polaroid/Polaroid.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import './Polaroid.css';
-import { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import "./Polaroid.css";
 
 /**
  * @param {string} url - Polaroid image url
  * @param {string} text - Polaroid legend and alt text.
  */
 function Polaroid(props) {
-
   const [turnLeft, setTurnleft] = useState(true);
-  const [classNames, setClassNames] = useState( turnLeft? "left" : "right");
+  const [classNames, setClassNames] = useState(turnLeft ? "left" : "right");
 
-  useEffect(() => setClassNames(turnLeft? "left" : "right"), [turnLeft])
+  useEffect(() => setClassNames(turnLeft ? "left" : "right"), [turnLeft]);
 
   return (
-    <div className={"polaroid " + classNames} onClick={() => setTurnleft(!turnLeft)}>
-        <img src={props.url} alt={props.text}></img>
-        <h3>{props.text}</h3>
-    </div >);
+    <div
+      className={"polaroid " + classNames}
+      onClick={() => setTurnleft(!turnLeft)}
+    >
+      <img src={props.url} alt={props.text}></img>
+      <h3>{props.text}</h3>
+    </div>
+  );
 }
 
 export default Polaroid;
