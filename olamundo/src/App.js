74d9bba21ecda9f123e786bccd6eea884381ef42
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-09-05 20:11:07 -0300

    adicionar exemplos extras de propriedades

--- olamundo/src/App.js
+++ olamundo/src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import Polaroid from './components/Polaroid';
 
 function App() {
+
+  function moveMove(){
+    console.log("I like to move it, move it!");
+    console.log("⊂(▀¯▀⊂)⊂(▀¯▀⊂)");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Polaroid url="http://jardinagembelasflores.comunidades.net/1423066980_08/104_decoracao6.jpg" text="Imagem Polaroid" >
+        <Polaroid url="http://jardinagembelasflores.comunidades.net/1423066980_08/104_decoracao6.jpg" 
+            text="Imagem Polaroid" shakeIt={moveMove} >
+          <h1 style={{color:'green'}}>Imagem Polaroid</h1>
         </Polaroid>
       </header>
     </div>
--- olamundo/src/components/Polaroid/Polaroid.jsx
+++ olamundo/src/components/Polaroid/Polaroid.jsx
@@ -4,6 +4,7 @@ import "./Polaroid.css";
 /**
  * @param {string} url - Polaroid image url
  * @param {string} text - Polaroid legend and alt text.
+ * @param {function} shakeIt - Polaroid event.
  */
 function Polaroid(props) {
   const [turnLeft, setTurnleft] = useState(true);
@@ -11,13 +12,25 @@ function Polaroid(props) {
 
   useEffect(() => setClassNames(turnLeft ? "left" : "right"), [turnLeft]);
 
+  let inner;
+
+  if (props.children) {
+    inner = props.children;
+  } else {
+    inner = <h3>{props.text}</h3>;
+  }
+
+  function handleClick() {
+    setTurnleft(!turnLeft);
+    if (props.shakeIt && props.shakeIt.call) {
+      props.shakeIt();
+    }
+  }
+
   return (
-    <div
-      className={"polaroid " + classNames}
-      onClick={() => setTurnleft(!turnLeft)}
-    >
+    <div className={"polaroid " + classNames} onClick={handleClick}>
       <img src={props.url} alt={props.text}></img>
-      <h3>{props.text}</h3>
+      {inner}
     </div>
   );
 }
