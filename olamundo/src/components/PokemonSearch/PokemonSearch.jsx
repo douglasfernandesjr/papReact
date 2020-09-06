fff158cbccf0b6fd1ba9aa9fcd33779ec285a5a2
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-09-06 11:21:46 -0300

    adicionar listagem simple

---
+++ olamundo/src/components/PokemonSearch/PokemonSearch.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+/**
+ * @param {string} url - Polaroid image url
+ * @param {string} text - Polaroid legend and alt text.
+ * @param {function} shakeIt - Polaroid event.
+ */
+function PokemonSearch() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => response.json()) // Transforma o retorno json em dados
+      .then((data) => {
+        setPokemonList(data.results);
+      });
+  }, []); // o [] garante que somente será executado uma única vez.
+
+  return (
+    <>
+      {pokemonList.map((item, i) => (
+        <div>
+          <h3>{item.name}</h3>
+          <a href={item.url} target="_blank">
+            Mais Informações
+          </a>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default PokemonSearch;
