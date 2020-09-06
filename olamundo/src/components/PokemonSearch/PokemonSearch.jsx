a67618438a99410924e519d139c9d1a77ee634cd
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-09-06 12:19:16 -0300

    adicionar filtro de valor

--- olamundo/src/components/PokemonSearch/PokemonSearch.jsx
+++ olamundo/src/components/PokemonSearch/PokemonSearch.jsx
@@ -1,23 +1,81 @@
 import React, { useState, useEffect } from "react";
 
+function mountApiUrl(selectedGen) {
+  let limit = 151;
+  let offset = 0;
+
+  switch (selectedGen) {
+    case "gen2":
+      limit = 100;
+      offset = 151;
+      break;
+    case "gen3":
+      limit = 135;
+      offset = 251;
+      break;
+    case "gen4":
+      limit = 107;
+      offset = 386;
+      break;
+    case "gen5":
+      limit = 156;
+      offset = 493;
+      break;
+    case "gen6":
+      limit = 72;
+      offset = 649;
+      break;
+    case "gen7":
+      limit = 88;
+      offset = 721;
+      break;
+    case "gen8":
+      limit = 81;
+      offset = 809;
+      break;
+    case "gen1":
+    default:
+      break;
+  }
+
+  return `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+}
+
 /**
  * @param {string} url - Polaroid image url
  * @param {string} text - Polaroid legend and alt text.
  * @param {function} shakeIt - Polaroid event.
  */
 function PokemonSearch() {
+  const [selectedGen, setSelectedGen] = useState("gen1");
   const [pokemonList, setPokemonList] = useState([]);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    fetch(mountApiUrl(selectedGen))
       .then((response) => response.json()) // Transforma o retorno json em dados
       .then((data) => {
         setPokemonList(data.results);
       });
-  }, []); // o [] garante que somente será executado uma única vez.
+  }, [selectedGen]);
+  function handleSelectedGenChange(event) {
+    setSelectedGen(event.target.value);
+  }
 
   return (
-    <> 
+    <>
+      <label>
+        Escolha a geração
+        <select value={selectedGen} onChange={handleSelectedGenChange}>
+          <option value="gen1">Geração I</option>
+          <option value="gen2">Geração II</option>
+          <option value="gen3">Geração III</option>
+          <option value="gen4">Geração IV</option>
+          <option value="gen5">Geração V</option>
+          <option value="gen6">Geração VI</option>
+          <option value="gen7">Geração VII</option>
+          <option value="gen8">Geração VIII</option>
+        </select>
+      </label>
       {pokemonList.map((item, i) => (
         <div key={i}>
           <h3>{item.name}</h3>
